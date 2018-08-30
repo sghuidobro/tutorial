383cff1770261ef1150cf10d20f65ba53b634aaa
sghuidobro <sghuidobro@gmail.com>
2018-08-30 12:52:11 -0300

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
-class StaticPagesController < ApplicationController
+	class StaticPagesController < ApplicationController
   def home
   end
 
   def help
+  end
+
+  def about
+  end
+
+  def contact
   end
 end
